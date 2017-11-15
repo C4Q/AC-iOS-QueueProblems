4d77ffc7141b935a832f97abd13450424b9b8b95
Melissa He <melissahe@ac.c4q.nyc>
2017-11-15 12:49:34 -0500

    Finished exercises 1-5

--- AC-iOS-QueuesExercises/QueueExercises.swift
+++ AC-iOS-QueuesExercises/QueueExercises.swift
@@ -11,13 +11,35 @@ import Foundation
 //1. Find the smallest element in a queue
 
 func smallestElement<T: Comparable>(in q: Queue<T>) -> T? {
-    return nil
+    guard !q.isEmpty else {
+        return nil
+    }
+    
+    var q = q
+    var smallestElement = q.peek()!
+    
+    while !q.isEmpty {
+        if smallestElement > q.peek()! {
+            smallestElement = q.deQueue()!
+        } else {
+            q.deQueue()
+        }
+    }
+    
+    return smallestElement
 }
 
 //2. Find the sum of a queue of Ints
 
 func sum(of q: Queue<Int>) -> Int {
-    return 0
+    var q = q
+    var sumOfQ = 0
+    
+    while !q.isEmpty {
+        sumOfQ += q.deQueue()!
+    }
+    
+    return sumOfQ
 }
 
 //3. Find out whether or not a queue is in sorted order from smallest to biggest
@@ -28,6 +50,18 @@ False example   (Back) 4 - 19 - 134 - 200 (Front)
 */
 
 func isSorted<T: Comparable>(q: Queue<T>) -> Bool {
+    var q = q
+    
+    while !q.isEmpty {
+        guard let currentQ = q.deQueue(), let nextQ = q.peek() else {
+            break
+        }
+        
+        if currentQ > nextQ {
+            return false
+        }
+    }
+    
     return true
 }
 
@@ -39,12 +73,45 @@ Sample Output:  (Back) 31 - 2 - 16 - 9 (Front)
  */
 
 func reversed<T>(q: Queue<T>) -> Queue<T> {
-    return q
+    var q = q
+    var reversedQ = Queue<T>()
+    var reversedArr: [T] = []
+    
+    while !q.isEmpty {
+        reversedArr.append(q.deQueue()!)
+    }
+    
+    while !reversedArr.isEmpty {
+        reversedQ.enQueue(reversedArr.popLast()!)
+    }
+    
+    return reversedQ
 }
 
-//5 Determine if two Queues are equal
+//5. Determine if two Queues are equal
 
 func areEqual<T: Equatable>(qOne: Queue<T>, qTwo: Queue<T>) -> Bool {
-    return false
+    var qOne = qOne
+    var qTwo = qTwo
+    var qArrOne: [T] = []
+    var qArrTwo: [T] = []
+    
+    while !qOne.isEmpty {
+        qArrOne.append(qOne.deQueue()!)
+    }
+    
+    while !qTwo.isEmpty {
+        qArrTwo.append(qTwo.deQueue()!)
+    }
+    
+    if qArrOne.count != qArrTwo.count {
+        return false
+    }
+    
+    if qArrOne != qArrTwo {
+        return false
+    }
+    
+    return true
 }
 
